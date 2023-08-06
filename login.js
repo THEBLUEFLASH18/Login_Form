40f72dfdafa2d203c431094a70270d737d01d3fa
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2023-08-06 16:33:14 -0400

    adding firebase initializer

---
+++ login.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAy4ZcpC1dFu31INK637buxdc1trjStJIA",
+  authDomain: "gaming-tournament-18.firebaseapp.com",
+  projectId: "gaming-tournament-18",
+  storageBucket: "gaming-tournament-18.appspot.com",
+  messagingSenderId: "394670526321",
+  appId: "1:394670526321:web:b6e60d71fd1e8ec46070d0",
+  measurementId: "G-4MR4WVTG3P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
