5b730ef1f50c7339ba1ea4f9f22432cd842839f0
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2023-08-07 13:48:43 -0400

    added a few corrections

--- signUp.js
+++ signUp.js
@@ -1,14 +1,15 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const app = initializeApp(firebaseConfig);
 
+const auth = getAuth(app);
 const apiKey = process.env.API_KEY;
 
 const firebaseConfig = {
@@ -21,10 +22,7 @@ const firebaseConfig = {
     measurementId: "G-KZBYC63QZN"
   };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
 
 const signUpForm = document.querySelector(".signup");
 signUpForm.addEventListener('submit', (e) =>{
