da693912a010b81c0d848d5f274d249516596af4
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2023-08-07 13:40:25 -0400

    Added import SDKs for javascript

--- login.js
+++ login.js
@@ -1,14 +1,12 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore, doc, getDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const apiKey = process.env.API_KEY;
 
 const firebaseConfig = {
@@ -19,23 +17,21 @@ const firebaseConfig = {
     messagingSenderId: "394670526321",
     appId: "1:394670526321:web:8fa3d856731a7d536070d0",
     measurementId: "G-KZBYC63QZN"
-  };
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
-
-
+const db = getFirestore(); // If you're using Firestore
 
 const signIn = document.querySelector("#signIn")
-signIn.addEventListener('submit', (e)=>{
-  e.preventDefault()
+signIn.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-  const email = signIn['signIn-email'].value
-  const password = signIn['signIn-password'].value
-  auth.signInWithEmailAndPassword(email, password)
-    .then(cred =>{
-      console.log(cred.user)
-    })
-})
+    const email = signIn['signIn-email'].value
+    const password = signIn['signIn-password'].value
+    signInWithEmailAndPassword(auth, email, password)
+        .then(cred => {
+            console.log(cred.user)
+        })
+});
